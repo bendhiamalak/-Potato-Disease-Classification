cae711fb0950edc06918ddbed92cb2154fa7638f
Malek Ben Dhia <malek.bendhia0@gmail.com>
2025-07-28 16:38:14 +0100

    frontend done !

--- api/main.py
+++ api/main.py
@@ -5,9 +5,26 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 from keras.layers import TFSMLayer
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins=[
+    "http://localhost",
+    "http://localhost:5500",  
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",  
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=["*"]
+)
 MODEL = TFSMLayer("../models/3", call_endpoint="serving_default")
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
@@ -31,7 +48,7 @@ async def predict(
 
     predicted_class = CLASS_NAMES[np.argmax(output)]
     confidence = float(np.max(output))
-
+    print("prediction is called from the front")
     return {
         "class": predicted_class,
         "confidence": confidence
